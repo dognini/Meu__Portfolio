b8877f083430db0f92084fda53072b49d604451d
Diogo Dognini Correa <diogo.dognini002@gmail.com>
2023-08-08 15:15:19 -0300

    arrumando o import do postModelo.module.css

--- src/componentes/postModelo/postModelo.jsx
+++ src/componentes/postModelo/postModelo.jsx
@@ -1,4 +1,4 @@
-import styles from "./PostModelo.module.css"
+import styles from "./postModelo.module.css"
 
 // eslint-disable-next-line react/prop-types
 export default function PostModelo({titulo, children }) {
